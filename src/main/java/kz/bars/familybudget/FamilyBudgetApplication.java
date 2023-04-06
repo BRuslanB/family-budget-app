c0e2d467fe33797a6bc36e22ce97050ac1f041fe
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2023-04-06 23:40:49 +0600

    #14 Issue. Add actuator.

--- src/main/java/kz/bars/familybudget/FamilyBudgetApplication.java
+++ src/main/java/kz/bars/familybudget/FamilyBudgetApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Family Budget API", version = "1.0.0", description = "Family Budget Information"))
+//@OpenAPIDefinition(info = @Info(title = "Family Budget API", version = "1.0.0", description = "Family Budget Information"))
 @SecurityScheme(name = "javainuseapi", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class FamilyBudgetApplication {
 
