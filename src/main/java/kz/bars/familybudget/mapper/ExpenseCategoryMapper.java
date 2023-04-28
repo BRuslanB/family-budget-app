c1a39ce77a7c5d040d31b5ffc813a3e669ccd5db
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2023-04-28 16:21:51 +0600

    #33 issue: added to function toFixed(2) and removing extra lines

--- src/main/java/kz/bars/familybudget/mapper/ExpenseCategoryMapper.java
+++ src/main/java/kz/bars/familybudget/mapper/ExpenseCategoryMapper.java
@@ -4,13 +4,9 @@ import kz.bars.familybudget.dto.ExpenseCategoryDto;
 import kz.bars.familybudget.model.ExpenseCategory;
 import org.mapstruct.Mapper;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface ExpenseCategoryMapper {
 
     ExpenseCategoryDto toDto(ExpenseCategory expenseCategory);
-//    ExpenseCategory toEntity(ExpenseCategoryDto expenseCategoryDto);
-//    List<ExpenseCategoryDto> toDtoList(List<ExpenseCategory> expenseCategories);
 
 }
--- src/main/java/kz/bars/familybudget/repository/UserRepo.java
+++ src/main/java/kz/bars/familybudget/repository/UserRepo.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
-
 @Repository
 @Transactional
 public interface UserRepo extends JpaRepository<User, Long> {
